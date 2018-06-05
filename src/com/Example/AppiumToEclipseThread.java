aad7800f903d992d531e733732496cf9cba04ee3
Anshuman Chatterjee <anshuman.chatterjee@sstsinc.com>
2018-06-05 18:12:10 +0530

    Changed code to support Intercept in future

--- src/com/Example/AppiumToEclipseThread.java
+++ src/com/Example/AppiumToEclipseThread.java
@@ -1,8 +1,11 @@
 package com.Example;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.CharBuffer;
 import java.nio.charset.StandardCharsets;
 
 public class AppiumToEclipseThread implements Runnable {
@@ -19,27 +22,42 @@ public class AppiumToEclipseThread implements Runnable {
 	public void run() {
 		try {
 			// Lets read from Eclipse Socket and write the request to Appium Socket
-			StringBuilder sb = new StringBuilder();
-			while (true) {
-				byte[] buffer = new byte[ProxyThread.BUFFER_SIZE];
-				int bytesRead = appiumIn.read(buffer);
+			StringBuffer sb = new StringBuffer();
+			String line = null;
+			BufferedReader br = new BufferedReader(new InputStreamReader(appiumIn, StandardCharsets.UTF_8));
 
-				if (bytesRead == -1)
+			// read headers
+			while ((line = br.readLine()) != null) {
+				sb.append(line);
+
+				byte[] lineBytes = (line + "\r\n").getBytes(StandardCharsets.UTF_8);
+				System.err.println("  <<  " + line);
+				eclipseOut.write(lineBytes);
+				if (line.isEmpty())
 					break;
-
-				String bytesToString = new String(buffer, StandardCharsets.UTF_8);
-
-				System.err.print("<<" + bytesToString);
-				sb.append(bytesToString);
-
-				eclipseOut.write(buffer, 0, bytesRead);
-
 			}
-			System.err.println();
-			// System.out.println("<<" + sb.toString());
+
+			System.err.println("Now will read Body");
+			// System.out.println(">>" + sb.toString());
 			eclipseOut.flush();
 
-			System.out.println("AppiumToEclipseThread completed.");
+			while (true) {
+				CharBuffer buffer = CharBuffer.allocate(ProxyThread.BUFFER_SIZE);
+				int bytesRead = br.read(buffer);
+
+				String bytesToString = new String(buffer.array());
+
+				System.err.println("  <<  " + bytesToString);
+				sb.append(bytesToString);
+
+				eclipseOut.write(bytesToString.getBytes(StandardCharsets.UTF_8), 0, bytesRead);
+
+				if (bytesRead < ProxyThread.BUFFER_SIZE)
+					break;
+			}
+
+			System.err.println("AppiumToEclipseThread completed.");
+			System.err.println();
 		} catch (IOException e) {
 
 			e.printStackTrace();
